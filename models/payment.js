27f6c7f083f218fd00a479ea21d79b83c64e88d3
unknown <dev23.mxpertz@gmail.com>
2024-05-02 17:08:23 +0530

    updated..

---
+++ models/payment.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const paymentSchema = mongoose.Schema({
+  Student_ID: {
+    type: mongoose.Types.ObjectId,
+    ref: "student"
+  },
+  Package_ID: {
+    type: mongoose.Types.ObjectId,
+    ref: "Package"
+  },
+  Purchase_Amount: {
+    type: Number,
+    required: true,
+  },
+  Booking_ID: {
+    type: mongoose.Types.ObjectId,
+    ref: "bookings"
+  },
+  Status: {
+    type: String,
+    required: true,
+  },
+  Method: {
+    type: String,
+    required: true,
+  },
+}, {
+  timestamps: true,
+});
+
+const payments = mongoose.model('payments', paymentSchema);
+module.exports = payments;
